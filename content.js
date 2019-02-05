aaf598d8a9bd26da683e16a45e70a9734cba3c5e
dmoneyballer <drew.gilliesbc@gmail.com>
2019-02-04 18:37:59 -0700

    working in Chrome, Edge and Firefox

---
+++ content.js
@@ -0,0 +1,23 @@
+
+
+
+// return highlited text
+function getSelectedText() {
+    var text = "";
+    if (typeof window.getSelection != "undefined") {
+        text = window.getSelection().toString();
+    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+function doSomethingWithSelectedText() {
+    var selectedText = getSelectedText();
+    if (selectedText) {
+        alert(selectedText);
+    }
+}
+
+document.onmouseup = doSomethingWithSelectedText;
+document.onkeyup = doSomethingWithSelectedText;
